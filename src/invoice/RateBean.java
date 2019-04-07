31554586d8d929c6d4aeff8a5bfc7bf84eded96c
Codeama <bukola_jimoh@yahoo.co.uk>
2019-04-07 11:59:44 +0100

    clean up

--- src/invoice/RateBean.java
+++ src/invoice/RateBean.java
@@ -7,13 +7,8 @@ package invoice;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.NumberBinding;
 import javafx.beans.binding.ObjectBinding;
-import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
