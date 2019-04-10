c596d94e7626bebacf61c14ee152674a4f1738bd
Codeama <bukola_jimoh@yahoo.co.uk>
2019-04-10 20:05:22 +0100

    query and display expenses

--- src/home/HomeController.java
+++ src/home/HomeController.java
@@ -8,6 +8,7 @@ package home;
 import business.manager.BusinessManager;
 import business.manager.ScreenChangeListener;
 import business.manager.ScreenHandler;
+import entity_classes.Expenses;
 import entity_classes.Invoices;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -35,6 +36,8 @@ public class HomeController implements Initializable, ScreenChangeListener {
     ScreenHandler screenController;
     private final NumberFormat currency = NumberFormat.getCurrencyInstance();
     @FXML private Label incomeLabel;
+    @FXML
+    private Label expensesLabel;
     
     //queries
      private final EntityManagerFactory entityManagerFactory =
@@ -45,28 +48,23 @@ public class HomeController implements Initializable, ScreenChangeListener {
     
     //Query query = entityManager.createNamedQuery("SELECT t FROM Invoices t ORDER BY t.id DESC");
     
-        private final TypedQuery<Invoices> getInvoiceAmount = 
-            entityManager.createQuery("SELECT t FROM Invoices t ORDER BY t.id DESC", Invoices.class);
+    private final TypedQuery<Invoices> getInvoiceAmount = 
+        entityManager.createQuery("SELECT t FROM Invoices t ORDER BY t.id DESC", Invoices.class);
+    
+    private final TypedQuery<Expenses> getExpenseAmount = 
+            entityManager.createQuery("SELECT e FROM Expenses e ORDER BY e.id DESC", Expenses.class);
+        
+        
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        incomeLabel.setText(currency.format(BigDecimal.ZERO.setScale(
-                2, RoundingMode.HALF_UP)));
-        
-        //BigDecimal current = query.setMaxResults(1).getSingleResult();
-        
-       List<Invoices> subTotals = getInvoiceAmount.setMaxResults(1).getResultList();
-       if(subTotals != null)
-       subTotals.forEach(invoice -> {
-           incomeLabel.setText(currency.format(invoice.getRunningTotal().setScale(
-                2, RoundingMode.HALF_UP)));
-           System.out.println(invoice.getRunningTotal());});
-
-        //System.out.println(getInvoiceAmount.getSingleResult().getRunningTotal());
-        //System.out.println(getInvoiceAmount.getResultList());
+        //INCOME
+        queryAndDisplayIncome();
+       
+       //EXPENSES
+       queryAndDisplayExpenses();
     }    
 
     @Override
@@ -82,6 +80,31 @@ public class HomeController implements Initializable, ScreenChangeListener {
     @FXML
     public void goToExpenses(){
         screenController.setScreen(BusinessManager.expensesID);
+    }
+    
+    private void queryAndDisplayIncome(){
+       incomeLabel.setText(currency.format(BigDecimal.ZERO.setScale(
+                2, RoundingMode.HALF_UP))); //display zero unless subTotals below is true
+       
+       List<Invoices> subTotals = getInvoiceAmount.setMaxResults(1).getResultList();
+       if(subTotals != null){
+       subTotals.forEach(invoice -> {
+           incomeLabel.setText(currency.format(invoice.getRunningTotal().setScale(
+                2, RoundingMode.HALF_UP)));
+           System.out.println(invoice.getRunningTotal());});
+       }
+    }
+    
+    private void queryAndDisplayExpenses(){
+        expensesLabel.setText(currency.format(BigDecimal.ZERO.setScale(
+                2, RoundingMode.HALF_UP)));
+       List<Expenses> expensesSubTotal = getExpenseAmount.setMaxResults(1).getResultList();
+       if(expensesSubTotal != null){
+           expensesSubTotal.forEach(expense -> {
+               expensesLabel.setText(currency.format(expense.getRunningTotal()));//.setScale(0, RoundingMode.CEILING)));
+           });
+       }
+
     }
     
 }
